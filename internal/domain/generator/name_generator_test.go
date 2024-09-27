03dc771cc0b9d31a5e741a7b4d51e5321a73152c
Paulo Rohling <paulohr.freecopias@gmail.com>
2024-09-26 23:37:00 -0300

    test: testing name generator

--- internal/domain/generator/name_generator_test.go
+++ internal/domain/generator/name_generator_test.go
@@ -2,11 +2,28 @@ package generator
 
 import (
 	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestNameGenerator_Generate(t *testing.T) {
-	nameGenerator := NewNameGenerator("https://api.livecursors.com", "api-key")
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("X-API-KEY") != "api-key" {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		if r.Method == http.MethodGet {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`{"name": "Paulo"}`))
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+	}))
+
+	httpClient := http.DefaultClient
+	nameGenerator := NewNameGenerator(httpClient, mockServer.URL, "api-key")
 	name, err := nameGenerator.Generate()
 	assert.NoError(t, err)
 	assert.NotEmpty(t, name)
